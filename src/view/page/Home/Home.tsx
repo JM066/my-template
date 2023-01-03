0f502de7a58e105dab067cd6d1e5ba62f2fde2a9
minalee <mina.lee066@gmail.com>
2023-01-03 18:10:07 +0700

    Test..

--- src/view/page/Home/Home.tsx
+++ src/view/page/Home/Home.tsx
@@ -1,16 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Button from '../../component/Button/Button'
 
 export default function Home() {
-    const [isStudentLogin, setIsStudentLogin] = useState<boolean>(true)
-
     return (
         <div>
-            <Button
-                color="secondary"
-                onclick={() => setIsStudentLogin((prev) => !prev)}
-            >
-                {isStudentLogin ? 'Student Login' : 'Instructor Login'}
+            <Button color="secondary" onclick={() => console.log()}>
+                Hi Template!
             </Button>
             <div className="flex flex-row"></div>
         </div>
